5aa9afda9dd10bfd76cdd679f66c18f57fa2fbb7
Tom Scott <tubbo@psychedeli.ca>
2013-08-12 00:28:35 -0400

    Configure uploaders to send files to S3

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,7 +2,7 @@
 
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
-  storage :file
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
