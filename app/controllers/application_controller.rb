507fc04df2664939b9f119128ac88406bd0f0e05
Tom Scott <tubbo@psychedeli.ca>
2013-08-11 20:52:27 -0400

    Migrate DB and begin integrating AA bugs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,9 @@ class ApplicationController < ActionController::Base
 
   private
   def handle_html_requests
-    return if request.xhr?
-    respond_to { |format| format.html { render 'application/index' } }
+    unless params[:controller] =~ /admin/
+      return if request.xhr?
+      respond_to { |format| format.html { render 'application/index' } }
+    end
   end
 end
