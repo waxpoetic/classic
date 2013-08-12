dbb764d8c42c2a1ffee121782983b5b5d5e6eaa7
Tom Scott <tubbo@psychedeli.ca>
2013-08-11 21:42:00 -0400

    Validate that Release has an Artist

--- app/models/release.rb
+++ app/models/release.rb
@@ -3,6 +3,7 @@ class Release < ActiveRecord::Base
   validates :name, presence: true
   validates :catalog_number, presence: true
   validates :released_on, presence: true
+  validates :artist, presence: true
 
   mount_uploader :cover_image, CoverUploader
 end
