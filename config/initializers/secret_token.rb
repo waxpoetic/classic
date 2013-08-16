18ce0c35b12eda5c20d92eb5005c098d6dba3470
Tom Scott <tubbo@psychedeli.ca>
2013-08-15 21:38:08 -0400

    Make the secret token secret

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-WaxPoetic::Application.config.secret_token = '5e7a2297a66da60ece5cd2f4283239eeda96c3495dbe32b337e359958449959c161461eb633fb864065da1f1c57412968e3b5e07444b8a47217e31eedce0b15a'
+WaxPoetic::Application.config.secret_token = ENV['RAILS_SECRET']
