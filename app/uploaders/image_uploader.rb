90e1d9a4ec6475fa6f6a92e552597396775c920d
Tom Scott <tubbo@psychedeli.ca>
2013-08-15 21:56:26 -0400

    Change path of images when they're uploaded.

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -6,7 +6,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "#{table_name}/#{model.id}/#{mounted_as}"
+  end
+
+  def table_name
+    model.class.to_s.underscore.pluralize
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
