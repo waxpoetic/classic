5c4cf09cbf075cde55e951307bc834898b169a4e
Tom Scott <tubbo@psychedeli.ca>
2013-08-24 21:21:29 -0400

    Order handles the data backing for the concept of a cart.

--- app/models/cart.rb
+++
@@ -1,4 +0,0 @@
-class Cart < ActiveRecord::Base
-  belongs_to :user
-  # attr_accessible :title, :body
-end
--- db/migrate/20130825003035_create_orders.rb
+++ db/migrate/20130825003035_create_orders.rb
@@ -2,7 +2,8 @@ class CreateOrders < ActiveRecord::Migration
   def change
     create_table :orders do |t|
       t.references :user
-
+      t.decimal :total, default: 0.00
+      t.boolean :is_checked_out, default: false
       t.timestamps
     end
     add_index :orders, :user_id
