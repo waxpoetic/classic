57d5eaf2c28357952cb1bf9b08e14a5786e7c440
Tom Scott <tubbo@psychedeli.ca>
2013-08-25 21:18:31 -0400

    Add search_params to artists

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,8 +2,21 @@ class ArtistsController < ApplicationController
   respond_to :json
 
   def index
-    @artists = Artist.all
+    @artists = Artist.where search_params
 
     respond_with @artists
+  end
+
+  def show
+    @artist = Artist.find params[:id]
+    render json: { errors: ["Artist not found."] } and return \
+      unless @artist.present?
+
+    respond_with @artist
+  end
+
+  private
+  def search_params
+    params.permit(:name)
   end
 end
