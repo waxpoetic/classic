32610221bf7d9e143a392e1a1865d7ba2e19da9d
Tom Scott <tubbo@psychedeli.ca>
2013-09-21 19:47:32 -0400

    Spec for order attrs and user data

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe User do
-  it "provides authentication to the product ordering system"
+  subject do
+    User.new \
+      email: 'test@example.com',
+      password: 'testMe123',
+      password_confirmation: 'testMe123'
+  end
+
+  it "authenticates by email and password" do
+    expect(subject.email).to be_present
+  end
+
+  it "authenticates by api token" do
+    expect(subject).to be_valid
+    expect(subject.authentication_token).to be_present
+  end
 end
--- spec/serializers/order_serializer_spec.rb
+++ spec/serializers/order_serializer_spec.rb
@@ -5,14 +5,16 @@ describe OrderSerializer do
   let(:order) { orders :wonderbars_discography }
   subject { OrderSerializer.new order }
 
-  it "exposes attributes id, total & is_checked_out" do
-    expect(subject.id).to eq(order.id)
-    expect(subject.total).to eq(order.total)
-    expect(subject.is_checked_out).to eq(order.is_checked_out)
+  [:id, :total, :is_checked_out].each do |attribute|
+    it "exposes #{attribute.to_s.humanize.downcase}" do
+      expect(subject.send(attribute)).to eq(order.send(attribute))
+    end
   end
+
   it "exposes user object" do
     expect(subject.products).to eq(order.products)
   end
+
   it "exposes products" do
     expect(subject.products).to eq(order.products)
   end
