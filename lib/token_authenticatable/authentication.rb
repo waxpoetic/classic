dfc21210208c2296b4e430d19fbfea04f3761beb
Tom Scott <tubbo@psychedeli.ca>
2013-09-21 19:51:51 -0400

    Fix naming

--- lib/token_authenticatable/authentication.rb
+++ lib/token_authenticatable/authentication.rb
@@ -5,27 +5,29 @@ require 'devise'
 # user_token in the params. Include this into any controller to require
 # user authentication for requests made inside the object.
 
-module UserAuthentication
-  protected
-  def authenticate_user_from_token!
-    authenticate_user! unless found_user && authenticated_with_token?
-    sign_in found_user, store: false
-  end
+module TokenAuthenticatable
+  module Authentication
+    protected
+    def authenticate_user_from_token!
+      authenticate_user! unless found_user && authenticated_with_token?
+      sign_in found_user, store: false
+    end
 
-  private
-  def authenticated_with_token?
-    Devise.secure_compare found_user.authentication_token, user_token
-  end
+    private
+    def authenticated_with_token?
+      Devise.secure_compare found_user.authentication_token, user_token
+    end
 
-  def found_user
-    @user ||= user_email && User.where(email: user_email).first
-  end
+    def found_user
+      @user ||= user_email && User.where(email: user_email).first
+    end
 
-  def user_email
-    params[:user_email].presence
-  end
+    def user_email
+      params[:user_email].presence
+    end
 
-  def user_token
-    params[:user_token].presence
+    def user_token
+      params[:user_token].presence
+    end
   end
 end
--- lib/token_authenticatable/token.rb
+++ lib/token_authenticatable/token.rb
@@ -1,27 +1,29 @@
 require 'active_support/concern'
 
 module TokenAuthenticatable
-  extend ActiveSupport::Concern
+  module Token
+    extend ActiveSupport::Concern
 
-  included do
-    before_validation :ensure_authentication_token
-    validate :authentication_token, presence: true
-  end
-
-  def ensure_authentication_token
-    self.authentication_token = generate_authentication_token \
-      if authentication_token.blank?
-  end
-
-  private
-  def generate_authentication_token
-    loop do
-      token = Devise.friendly_token
-      return token unless token_taken? token
+    included do
+      before_validation :ensure_authentication_token
+      validate :authentication_token, presence: true
     end
-  end
 
-  def token_taken? token
-    User.where(authentication_token: token).any?
+    def ensure_authentication_token
+      self.authentication_token = generate_authentication_token \
+        if authentication_token.blank?
+    end
+
+    private
+    def generate_authentication_token
+      loop do
+        token = Devise.friendly_token
+        return token unless token_taken? token
+      end
+    end
+
+    def token_taken? token
+      User.where(authentication_token: token).any?
+    end
   end
 end
