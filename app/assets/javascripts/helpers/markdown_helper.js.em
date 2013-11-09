4be8ba0f4aaa3fcfeb405c6228f33b815a0547a7
Tom Scott <tubbo@psychedeli.ca>
2013-11-09 16:59:29 -0500

    Fix seeds and tackle issue in Markdown helper when text is nil

--- app/assets/javascripts/helpers/markdown_helper.js.em
+++ app/assets/javascripts/helpers/markdown_helper.js.em
@@ -1,3 +1,4 @@
 Ember.Handlebars.registerBoundHelper 'markdown', (text) ->
+  return new Ember.Handlebars.SafeString "" unless text.length > 0
   markdown = new Showdown.converter()
   new Ember.Handlebars.SafeString "<div class=\"markdown\">#{markdown.makeHtml(text)}</div>"
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,19 +10,31 @@ unless Artist.any?
   rnd = Artist.create name: "Research & Development"
   subatomica = Artist.create name: "Subatomica"
 
-  just_the_start = wonder_bars.releases.create \
-    name: "Just The Start EP", released_on: 2.years.ago.to_date
-  shuffle_not = rnd.releases.create \
-    name: "Shuffle Not EP", released_on: 1.month.ago.to_date
-  cloud_shadows = subatomica.releases.create \
-    name: "Cloud Shadows EP", released_on: 1.month.from_now.to_date
+  just_the_start = wonder_bars.releases.build \
+    name: "Just The Start EP",
+    released_on: 2.years.ago.to_date,
+    description: "Debut from Philly's favorite house band"
+  shuffle_not = rnd.releases.build \
+    name: "Shuffle Not EP",
+    released_on: 1.month.ago.to_date,
+    description: "Debut from the mythical and mysterious R&D"
+  cloud_shadows = subatomica.releases.build \
+    name: "Cloud Shadows EP",
+    released_on: 1.month.from_now.to_date,
+    description: "The long-awaited remix EP of this amazing track"
 
   if just_the_start.save && shuffle_not.save && cloud_shadows.save
     shuffle_not_wavs = shuffle_not.products.create \
-      name: "WAV download", price: 3.99
+      name: "WAV download",
+      price: 3.99
     cloud_shadows_wavs = cloud_shadows.products.create \
-      name: "WAV download", price: 9.99
+      name: "WAV download",
+      price: 9.99
     shuffle_not_ose = shuffle_not.products.create \
-      name: "Open-Source", price: 9.99
+      name: "Open-Source",
+      price: 9.99
+
+    raise "whoops!" unless shuffle_not_wavs && cloud_shadows_wavs && shuffle_not_ose
   end
 end
+
