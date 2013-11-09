d806e60ab5f0464b72a7c5e00dc3a6886ab1c4e1
Tom Scott <tubbo@psychedeli.ca>
2013-11-09 17:11:25 -0500

    Don't redirect on Devise requests

--- lib/handle_html_requests.rb
+++ lib/handle_html_requests.rb
@@ -5,14 +5,14 @@ module HandleHtmlRequests
 
   protected
   def handle_html_requests
-    return if admin_interface? || json_request?
+    return if json_request? || whitelisted?
     return if request.xhr?
     respond_to { |format| format.html { render 'application/index' } }
   end
 
   private
-  def admin_interface?
-    params[:controller] =~ /admin/
+  def whitelisted?
+    params[:controller] =~ /admin|devise/
   end
 
   def json_request?
