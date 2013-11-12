8faf53eb841e5acae89635e3f5b0f2dad765b631
Tom Scott <tubbo@psychedeli.ca>
2013-11-11 23:14:56 -0500

    Move concerns to app/controllers/concerns

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,12 @@
-require 'handle_html_requests'
-require 'user_finder'
+require 'concerns/handle_html_requests'
+require 'concerns/user_finder'
 
 # Methods inherited by all controllers. Enables optional token
 # authentication, handling HTML requests, and finding users by
 # current_user or ID.
 
 class ApplicationController < ActionController::Base
-  include HandleHtmlRequests
-  include UserFinder
+  include HandleHtmlRequests, UserFinder
 
   protect_from_forgery
 end
