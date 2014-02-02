3406ce05f0a6e8e2171a61feeada78d0d5c51a3d
Tom Scott <tscott@telvue.com>
2014-02-02 03:25:38 -0500

    Use assets bucket

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -4,7 +4,7 @@ if defined? AssetSync
     config.fog_provider = 'AWS'
     config.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
     config.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
-    config.fog_directory = 'files.waxpoeticrecords.com'
+    config.fog_directory = 'assets.waxpoeticrecords.com'
 
     # Increase upload performance by configuring your region
     config.fog_region = 'us-east-1'
