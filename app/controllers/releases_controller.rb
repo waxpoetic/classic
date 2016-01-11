b2fa61f70c58efa4fb1511aaffe3c70ef2b873ac
Tom Scott <tscott@weblinc.com>
2016-01-11 18:01:37 -0500

    Add test-unit to Gemfile and reverse release ordering

--- app/controllers/releases_controller.rb
+++ app/controllers/releases_controller.rb
@@ -2,7 +2,7 @@ class ReleasesController < ApplicationController
   respond_to :json
 
   def index
-    @releases = Release.where(search_params).order 'released_at DESC'
+    @releases = Release.where(search_params).order 'released_on DESC'
 
     respond_with @releases
   end
